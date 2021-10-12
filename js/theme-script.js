edc83f6c80f6d94c83fb2651cb88b30d255fedbe
Upwork <freelancingonupwork@gmail.com>
2021-10-12 13:55:58 +0530

    initial commit

---
+++ js/theme-script.js
@@ -0,0 +1,540 @@
+/* ------------------------------------------------
+  Project:   Oveltyshop - ECommerce Responsive HTML5 Template
+  Author:    ThemeHt
+------------------------------------------------ */
+/* ------------------------
+    Table of Contents
+
+  1. Predefined Variables
+  2. Preloader  
+  3. Dropdown  
+  4. FullScreen
+  5. Slit Slider
+  6. Owl carousel
+  7. Magnific Popup
+  8. Isotope
+  9. Scroll to top
+  10. Banner Section
+  11. Fixed Header
+  12. Text Color, Background Color And Image
+  13. Contact Form
+  14. Masonry
+  15. Countdown
+  16. Cart
+  17. Slick Slider
+  18. Parallax
+  19. Range Slider
+  20. Nice Select
+  21. Wow Animation
+  22. HT Window load and functions
+  
+
+------------------------ */
+
+"use strict";
+
+/*------------------------------------
+  HT Predefined Variables
+--------------------------------------*/
+var $window = $(window),
+    $document = $(document),
+    $body = $('body'),
+    $fullScreen = $('.fullscreen-banner') || $('.section-fullscreen'),
+    $halfScreen = $('.halfscreen-banner');
+
+
+//Check if function exists
+$.fn.exists = function () {
+  return this.length > 0;
+};
+
+
+/*------------------------------------
+  HT PreLoader
+--------------------------------------*/
+function preloader() {
+   $('#ht-preloader').fadeOut();
+};
+
+
+/*------------------------------------
+  HT Dropdown
+--------------------------------------*/  
+function dropdown() {
+    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
+  if (!$(this).next().hasClass('show')) {
+    $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+  }
+  var $subMenu = $(this).next(".dropdown-menu");
+  $subMenu.toggleClass('show');
+
+  $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+    $('.dropdown-submenu .show').removeClass("show");
+  });
+
+  return false;
+});
+};
+
+
+/*------------------------------------
+  HT FullScreen
+--------------------------------------*/
+function fullScreen() {
+    if ($fullScreen.exists()) {
+        $fullScreen.each(function () {
+        var $elem = $(this),
+        elemHeight = $window.height();
+        if($window.width() < 768 ) $elem.css('height', elemHeight/ 1);
+        else $elem.css('height', elemHeight);
+        });
+        }
+        if ($halfScreen.exists()) {
+        $halfScreen.each(function () {
+        var $elem = $(this),
+        elemHeight = $window.height();
+        $elem.css('height', elemHeight / 1.5);
+        });
+    }
+};
+
+/*------------------------------------
+  HT Slit Slider
+--------------------------------------*/
+function slitslider() {
+  var Page = (function () {
+    var $navArrows = $('#nav-arrows'),
+      $nav = $('#nav-dots > span'),
+      slitslider = $('#slider').slitslider({
+        onBeforeChange: function (slide, pos) {
+          $nav.removeClass('nav-dot-current');
+          $nav.eq(pos).addClass('nav-dot-current');
+        }
+      }),
+      init = function () {
+        initEvents();
+      },
+      initEvents = function () {
+        // add navigation events
+        $navArrows.children(':last').on('click', function () {
+          slitslider.next();
+          return false;
+        });
+        $navArrows.children(':first').on('click', function () {
+          slitslider.previous();
+          return false;
+        });
+        $nav.each(function (i) {
+          $(this).on('click', function (event) {
+            var $dot = $(this);
+            if (!slitslider.isActive()) {
+              $nav.removeClass('nav-dot-current');
+              $dot.addClass('nav-dot-current');
+            }
+            slitslider.jump(i + 1);
+            return false;
+          });
+        });
+      };
+    return {
+      init: init
+    };
+  })();
+  Page.init();
+};
+
+
+/*------------------------------------
+  HT Owl Carousel
+--------------------------------------*/
+function owlcarousel() {
+$('.owl-carousel').each( function() {
+  var $carousel = $(this);
+  $carousel.owlCarousel({
+      items : $carousel.data("items"),
+      slideBy : $carousel.data("slideby"),
+      center : $carousel.data("center"),
+      loop : true,
+      margin : $carousel.data("margin"),
+      dots : $carousel.data("dots"),
+      nav : $carousel.data("nav"),      
+      autoplay : $carousel.data("autoplay"),
+      autoplayTimeout : $carousel.data("autoplay-timeout"),
+      navText : [ '<span class="flaticon-back"><span>', '<span class="flaticon-next"></span>' ],
+      responsive: {
+        0:{items: $carousel.data('xs-items') ? $carousel.data('xs-items') : 1},
+        576:{items: $carousel.data('sm-items')},
+        768:{items: $carousel.data('md-items')},
+        1024:{items: $carousel.data('lg-items')},
+        1200:{items: $carousel.data("items")}
+      },
+  });
+});
+};
+
+
+/*------------------------------------
+  HT Magnific Popup
+--------------------------------------*/
+function magnificpopup() {
+$('.popup-gallery').magnificPopup({
+    delegate: 'a.popup-img',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+      }
+    }
+  });
+if ($(".popup-youtube, .popup-vimeo, .popup-gmaps").exists()) {
+     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+          disableOn: 700,
+          type: 'iframe',
+          mainClass: 'mfp-fade',
+          removalDelay: 160,
+          preloader: false,
+          fixedContentPos: false
+    });
+  }
+
+};
+        
+
+/*------------------------------------
+  HT Isotope
+--------------------------------------*/ 
+function isotope() {
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows',
+  });
+
+  // filter functions
+  var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function() {
+      var number = $(this).find('.number').text();
+      return parseInt( number, 10 ) > 50;
+    },
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+
+  // bind filter button click
+  $('.portfolio-filter').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+
+
+  // change is-checked class on buttons
+  $('.portfolio-filter').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+};
+
+
+/*------------------------------------
+  HT Scroll to top
+--------------------------------------*/
+function scrolltop() {
+  var pxShow = 300,
+    goTopButton = $(".scroll-top")
+    // Show or hide the button
+  if ($(window).scrollTop() >= pxShow) goTopButton.addClass('scroll-visible');
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= pxShow) {
+      if (!goTopButton.hasClass('scroll-visible')) goTopButton.addClass('scroll-visible')
+    } else {
+      goTopButton.removeClass('scroll-visible')
+    }
+  });
+  $('.smoothscroll').on('click', function (e) {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+};
+
+ /*------------------------------------
+  HT Banner Section
+--------------------------------------*/
+function headerheight() {
+  $('.fullscreen-banner .align-center').each(function(){
+    var headerHeight=$('.header').height();
+    // headerHeight+=15; // maybe add an offset too?
+    $(this).css('padding-top',headerHeight+'px');
+  });
+};
+
+
+/*------------------------------------
+  HT Fixed Header
+--------------------------------------*/
+function fxheader() {
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= 100) {
+      $('#header-wrap').addClass('fixed-header');
+    } else {
+      $('#header-wrap').removeClass('fixed-header');
+    }
+  });
+  $(window).on('scroll', function () {
+    if ($window.width() > 992) $('.side-navbar #header-wrap').removeClass('fixed-header');
+  });
+};
+
+
+/*------------------------------------------
+  HT Text Color, Background Color And Image
+---------------------------------------------*/
+function databgcolor() {
+    $('[data-bg-color]').each(function(index, el) {
+     $(el).css('background-color', $(el).data('bg-color'));  
+    });
+    $('[data-text-color]').each(function(index, el) {
+     $(el).css('color', $(el).data('text-color'));  
+    });
+    $('[data-bg-img]').each(function() {
+     $(this).css('background-image', 'url(' + $(this).data("bg-img") + ')');
+    });
+};
+
+
+/*------------------------------------
+  HT Contact Form
+--------------------------------------*/
+function contactform() { 
+    $('#contact-form').validator();
+
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
+
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+        var url = "php/contact.php";
+
+        // POST values in the background the the script URL
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: $(this).serialize(),
+            success: function (data)
+            {
+            // data = JSON object that contact.php returns
+
+            // we recieve the type of the message: success x danger and apply it to the 
+            var messageAlert = 'alert-' + data.type;
+            var messageText = data.message;
+
+            // let's compose Bootstrap alert box HTML
+            var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+            
+            // If we have messageAlert and messageText
+            if (messageAlert && messageText) {
+                // inject the alert to .messages div in our form
+                $('#contact-form').find('.messages').html(alertBox).show().delay(2000).fadeOut('slow');
+                // empty the form
+                $('#contact-form')[0].reset();
+            }
+          }
+        });
+        return false;
+    }
+ })    
+};
+
+
+/*------------------------------------
+  HT Masonry
+--------------------------------------*/
+function masonry () {
+  var $masonry = $('.masonry'),
+      $itemElement = '.masonry-brick',
+      $filters = $('.portfolio-filter');  
+      if ($masonry.exists()) {
+        $masonry.isotope({
+          resizable: true,
+          itemSelector: $itemElement,
+        });
+
+         // bind filter button click
+     $filters.on( 'click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+         $masonry.isotope({ filter: filterValue });
+      });
+   }  
+};
+
+
+/*------------------------------------
+  HT Countdown
+--------------------------------------*/
+function countdown() {
+  $('.countdown').each(function () {
+    var $this = $(this),
+      finalDate = $(this).data('countdown');
+    $this.countdown(finalDate, function (event) {
+      $(this).html(event.strftime('<li><span>%-D</span><p>Days</p></li>' + '<li><span>%-H</span><p>Hours</p></li>' + '<li><span>%-M</span><p>Minutes</p></li>' + '<li><span>%S</span><p>Seconds</p></li>'));
+    });
+  });
+};
+
+
+/*------------------------------------
+  HT Cart
+--------------------------------------*/
+function cart () {
+    $('#header-cart-btn').on('click', function () {
+        $('body').toggleClass('cart-data-open');
+    })    
+};
+
+/*------------------------------------
+  HT Slick Slider
+--------------------------------------*/
+function slickslider() {
+  $(".vertical").slick({
+    dots: false,
+    vertical: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+  $('.slick3').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    dots: true,
+    appendDots: $('.slick3-dots-main'),
+    dotsClass: 'slick3-dots',
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    arrows: false,
+    customPaging: function (slick, index) {
+      var portrait = $(slick.$slides[index]).data('thumb');
+      return '<img src=" ' + portrait + ' "/><div class="slick3-dot-overlay"></div>';
+    },
+  });
+  $('.btn-product-up').on('click', function (e) {
+    e.preventDefault();
+    var numProduct = Number($(this).next().val());
+    if (numProduct > 1) $(this).next().val(numProduct - 1);
+  });
+  $('.btn-product-down').on('click', function (e) {
+    e.preventDefault();
+    var numProduct = Number($(this).prev().val());
+    $(this).prev().val(numProduct + 1);
+  });
+};
+
+
+
+/*------------------------------------
+  HT Parallax
+--------------------------------------*/
+function parallax() {
+  $(".parallaxie").parallaxie({
+      speed: 0.4,
+      offset: 0,
+  });
+};
+
+
+/*------------------------------------
+  HT Range Slider
+--------------------------------------*/
+function rangeslider() {
+  $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+};
+
+
+/*------------------------------------
+  HT Nice Select
+--------------------------------------*/
+function niceSelect() {
+  $('select').niceSelect();
+}
+
+/*------------------------------------
+  HT Wow Animation
+--------------------------------------*/
+function wowanimation() {
+    var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0,
+        mobile: false,
+        live: true
+    });
+    wow.init();
+}
+
+
+/*------------------------------------
+  HT Window load and functions
+--------------------------------------*/
+$(document).ready(function() {
+    dropdown(),
+    owlcarousel(),
+    fullScreen(),
+    slitslider(),
+    magnificpopup(),
+    scrolltop(),
+    headerheight()
+    fxheader(),
+    databgcolor(),  
+    contactform(),
+    countdown(),
+    cart(),
+    slickslider(),
+    parallax(),
+    rangeslider(),
+    niceSelect();
+});
+
+
+$window.resize(function() {
+    fullScreen();
+});
+
+
+$(window).on('load', function() {
+    preloader(),
+    isotope(),
+    masonry(),
+    wowanimation();
+});
+
+  
